4af911ee299ac9cc910e8deebab48e6f2ca1d9a7
skate090961 <m4xwork@yandex.ru>
2024-01-25 18:00:11 +0400

    feat: add button ui

---
+++ src/common/components/Button/Button.tsx
@@ -0,0 +1,43 @@
+import { ComponentPropsWithoutRef, ElementType } from 'react'
+import { ClipLoader } from 'react-spinners'
+
+import { Typography } from '@/common/components/Typography'
+
+import s from './Button.module.scss'
+
+type ButtonVariant = 'primary' | 'secondary' | 'tertiary'
+export type ButtonProps<T extends ElementType = 'button'> = {
+  as?: T
+  fullWidth?: boolean
+  isLoading?: boolean
+  variant?: ButtonVariant
+} & ComponentPropsWithoutRef<T>
+
+export const Button = <T extends ElementType = 'button'>(
+  props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
+) => {
+  const {
+    as: Component = 'button',
+    children,
+    className,
+    fullWidth,
+    isLoading,
+    variant = 'primary',
+    ...rest
+  } = props
+  const finalClassName = `${s.button} ${isLoading ? s.loading : ''} ${s[variant]} ${
+    fullWidth ? s.fullWidth : ''
+  }  ${className}`
+
+  return (
+    <Component className={finalClassName} {...rest}>
+      {isLoading ? (
+        <ClipLoader color={'#fff'} size={20} />
+      ) : (
+        <Typography className={s.typography} variant={'subtitle2'}>
+          {children}
+        </Typography>
+      )}
+    </Component>
+  )
+}
